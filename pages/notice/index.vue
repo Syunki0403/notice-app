6c3807f7f0aec8c4b558fa9694134b202290ad37
Syunki0403 <blackapple0403@gmail.com>
2021-04-29 18:32:55 +0900

    タイトル変更

--- pages/notice/index.vue
+++ pages/notice/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full md:max-w-3xl mx-auto pt-20">
     <h2 class="font-sans text-lg text-gray-800 text-center text-3xl mb-10">
-      お知らせ
+      お知らせページ
     </h2>
     <div class="mb-20">
       <div v-for="(item, index) in items" :key="index" class="bg-yellow-100 border-t-4 border-yellow-500 rouded-b text-teal-900 shadow-md mb-5">
